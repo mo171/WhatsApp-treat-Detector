6f37f89e1af42e08ce5b2612b9988413e8487b7e
Movin Dsouza <75006899+mo171@users.noreply.github.com>
2025-09-30 13:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def result_image():
 # RUN THE MAIN FILE
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
    
  
